fc655ca92959cb3e69b8ee8fa48cd227e39b1cfc
Billykassam94 <127251299+Billykassam94@users.noreply.github.com>
2023-04-06 16:09:59 +0100

    changed code layout
    removed code from main method to dbconnector class. created new method

--- src/homework4and5/DBConnector.java
+++ src/homework4and5/DBConnector.java
@@ -4,16 +4,22 @@
  */
 package homework4and5;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
  * @author nabil
  */
-public class DBConnector {
+public class DBConnector{
     
      // final = constant, cannot change
     private final String DB_URL = "jdbc:mysql://localhost";
@@ -57,6 +63,104 @@ public class DBConnector {
     }
     
     
+    public void addData() throws SQLException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException{
+        
+        
+        
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+
+        // buffered reader to read csv file
+        BufferedReader myReader1 = new BufferedReader(new FileReader("C:\\Users\\nabil\\Desktop\\Software development\\Programming Obj.-Oriented Approach\\Term 2\\Homework\\newData.csv"));
+
+        // array list to store elements
+        ArrayList<String> elements = new ArrayList<>();
+        
+        String line1 = null;
+
+        myReader1.readLine();
+        //loop to read all lines.
+        while ((line1 = myReader1.readLine()) != null) {
+            //splits each line into elements
+            String[] row = line1.split(",");
+//            System.out.println(row[1]);
+            elements.add(row[1]);
+        }
+
+        myReader1.close();
+
+        
+        
     
+        
+        try {
+
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost", "pooa", "pooa");
+            Statement stmt = conn.createStatement();
+            stmt.execute("USE homework5");
+            conn.setAutoCommit(false);
+
+            String sql = "INSERT INTO orders(Invoice,StockCode,Description,Quantity,InvoiceDate,Price,Customer,Country) VALUES(?,?,?,?,?,?,?,?)";
+
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            BufferedReader myReader = new BufferedReader(new FileReader("C:\\Users\\nabil\\Desktop\\Software development\\Programming Obj.-Oriented Approach\\Term 2\\Homework\\newData.csv"));
+
+            String line = myReader.readLine();
+            
+            myReader.readLine();
+            while (line  != null) {
+                // string array values that splits line by commas.
+                String[] values = line.split(",");
+
+                String Invoice = values[0];
+                String StockCode = values[1];
+                String Description = values[2];
+                String Quantity = values[3];
+                String InvoiceDate = values[4];
+                String Price = values[5];
+                String Customer = values[6];
+                String Country = values[7];
+
+                statement.setString(1, Invoice);
+                statement.setString(2, StockCode);
+                statement.setString(3, Description);
+                statement.setString(4, Quantity);
+                statement.setString(5, InvoiceDate);
+                statement.setString(6, Price);
+                statement.setString(7, Customer);
+                statement.setString(8, Country);
+                statement.addBatch();
+
+            }
+            
+            int count = 0;
+            
+            //assigns variable to the amount of elemnts by multiplying total rows by 8
+            int batchSize = elements.size() * 8;
+            
+            //if statement to excute batch until total amount is complete
+            if (count == batchSize) {
+                statement.executeBatch();
+                count++;
+            }
+
+            myReader.close();
+            statement.executeBatch();
+            conn.commit();
+            conn.close();
+
+            System.out.println("Data Imported");
+
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+
+    
+         
+           
+        
+    
+    }
     
 }
