3a2043334d55293b244048a8ee1ed8f18f39301b
Billykassam94 <127251299+Billykassam94@users.noreply.github.com>
2023-04-06 16:17:11 +0100

    removed unused imports

--- src/homework4and5/Homework4and5.java
+++ src/homework4and5/Homework4and5.java
@@ -4,16 +4,9 @@
  */
 package homework4and5;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 
 /**
  *
